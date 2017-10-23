5a1fc7dce4f6fea73c055a50baed51107240ac21
Guilherme Goettems Schneider <guigs81@gmail.com>
2017-10-23 16:40:31 -0200

    Allow passing params to RocketfuelApi::Service#get_all

--- spec/importeur/data_sources/rocketfuel_spec.rb
+++ spec/importeur/data_sources/rocketfuel_spec.rb
@@ -9,18 +9,30 @@ RSpec.describe Importeur::DataSources::Rocketfuel do
   let(:resource_resource)  { instance_double(RocketfuelApi::Resource::Company) }
 
   before do
-    expect(rocketfuel_service).to receive(:get_all).and_return([resource_resource])
+    allow(rocketfuel_service).to receive(:get_all).and_return([resource_resource])
   end
 
   describe '#items' do
     it 'returns items' do
       expect(data_source.items).to eq([resource_resource])
+      expect(rocketfuel_service).to have_received(:get_all).with({})
     end
   end
 
   describe '#dataset_unique_id' do
     it 'returns a hash' do
       expect(data_source.dataset_unique_id).to eq('a321d32f28c72c4723523a586f0424d6')
+    end
+  end
+
+  context 'with params' do
+    subject(:data_source) { described_class.new(rocketfuel_service, params) }
+
+    let(:params) { { 'filter' => 'value' } }
+
+    it 'pass params to #get_all and returns items' do
+      expect(data_source.items).to eq([resource_resource])
+      expect(rocketfuel_service).to have_received(:get_all).with(params)
     end
   end
 end
